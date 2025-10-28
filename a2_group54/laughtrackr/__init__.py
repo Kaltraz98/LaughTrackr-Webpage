a4a1ecd730589ebe4beaaae4ce0789eb834d4dbf
Kat <katherinepyne@gmail.com>
2025-10-28 18:28:07 +1000

    User login function fixes

--- a2_group54/laughtrackr/__init__.py
+++ a2_group54/laughtrackr/__init__.py
@@ -6,18 +6,27 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    # for displaying forms quickly
     Bootstrap5(app)
-
-    # secret key for the session object
     app.secret_key = 'somerandomvalue'
-
-    # Configue and initialise DB
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comedydb.sqlite'
     db.init_app(app)
-    
-    # add Blueprints
+
+    from flask_login import LoginManager, current_user
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.logged_in'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    @app.context_processor
+    def inject_user():
+        return dict(current_user=current_user)
+
+    # Blueprints
     from . import index
     app.register_blueprint(index.mainbp)
     from . import ComedyEvents
