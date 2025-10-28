c36666f85de777a27e5da93c196a8f0c218c113e
kaltraz98 <katherinepyne@gmail.com>
2025-10-28 22:40:53 +1000

    Updated the Database to fix issues with carasouel

--- a2_group54/laughtrackr/index.py
+++ a2_group54/laughtrackr/index.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from .models import *
+from . import db
 
 mainbp = Blueprint('main', __name__)  
 
 @mainbp.route('/')
 def index():
-    return render_template('IndexPage.html')
+    events = Event.query.all()
+    for event in events:
+        event.image = event.image.strip()
+    db.session.commit()
+    return render_template('IndexPage.html', events=events)
